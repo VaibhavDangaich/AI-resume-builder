9fda98eeb0f70a7c92c0faebcf8579e4f3ad048a
VaibhavDangaich <Vai1234dang@gmail.com>
2025-03-25 14:10:23 +0530

    ----

--- src/dashboard/resume/components/forms/EducationForm.jsx
+++ src/dashboard/resume/components/forms/EducationForm.jsx
@@ -1,17 +1,48 @@
 import React, { useState } from 'react'
 
 function EducationForm() {
-    const [educationalList,setEducationalList]=useState([{
+    const [educationalList, setEducationalList] = useState([{
+        universityName: '',
         degree: '',
-        institution: '',
-        startYear: '',
-        endYear: ''
+        major: '',
+        startDate: '',
+        endDate: '',
+        description: ''
     }])
+
+    const handleChange = (e, index) => {
+
+    }
     return (
 
         <div className='p-5 shadow-lg rounded-r-4xl border-t-purple-400 border-t-4 mt-10'>
             <h2 className='font-bold text-lg'>Education</h2>
             <p>Add your educational details</p>
+            <div>
+                {
+                    educationalList.map((edu, index) => (
+                        <div>
+                            <div>
+                                <div>
+                                    <label>University Name</label>
+                                    <input name='universityName' type='text' onChange={(e) => handleChange(e, index)}></input>
+                                </div>
+                                <div>
+                                    <label>Degree</label>
+                                    <input name='universityName' type='text' onChange={(e) => handleChange(e, index)}></input>
+                                </div>
+                                <div>
+                                    <label>University Name</label>
+                                    <input name='universityName' type='text' onChange={(e) => handleChange(e, index)}></input>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    ))
+                }
+            </div>
         </div>
 
     )
