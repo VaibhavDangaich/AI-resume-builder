4e1922f40edff95337419ad2119e07384e7b1a44
VaibhavDangaich <Vai1234dang@gmail.com>
2025-04-02 00:50:49 +0530

    working on landing page

--- src/dashboard/resume/components/ThemeColor.jsx
+++ src/dashboard/resume/components/ThemeColor.jsx
@@ -7,9 +7,10 @@ import {
 import { Button } from '@/components/ui/button'
 import { LayoutGrid } from 'lucide-react'
 import { ResumeInfoContext } from '@/context/ResumeInfoContext'
-import { useContext,useState } from 'react'
+import { useContext, useState } from 'react'
 import GlobalApi from './../../../../Service/GlobalApi'
 import { useParams } from 'react-router-dom'
+import { Input } from '@/components/ui/input'
 
 
 function ThemeColor() {
@@ -19,46 +20,54 @@ function ThemeColor() {
         "#33FF71", "#3371FF", "#A1FF33", "#33A1FF", "#F5733F",
         "#5733FF", "#33FF5A", "#5A33FF", "#FF335A", "#335AFF"
     ]
-const {resumeInfo,setResumeInfo}=useContext(ResumeInfoContext);
-const [selectedColor,setSelectedColor]=useState();
-const {resumeId}=useParams();
-const onColorSelect=(color)=>{
-    setSelectedColor(color);
-    setResumeInfo({
-        ...resumeInfo,
-        themeColor: color
-    })
-    const data={
-        data:{
+    const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext);
+    const [selectedColor, setSelectedColor] = useState();
+    const { resumeId } = useParams();
+    const onColorSelect = (color) => {
+        setSelectedColor(color);
+        setResumeInfo({
+            ...resumeInfo,
             themeColor: color
+        })
+        const data = {
+            data: {
+                themeColor: color
+            }
         }
-    }
-    GlobalApi.updateResumeDetail(resumeId,data).then(resp=>{
-        console.log(resp);
-        toast.success("Theme color updated");
-    })
+        GlobalApi.updateResumeDetail(resumeId, data).then(resp => {
+            console.log(resp);
+            toast.success("Theme color updated");
+        })
 
-}
+    }
     return (
         <Popover>
             <PopoverTrigger>
                 <Button variant="outline" size="sm" className="flex gap-2 cursor-pointer"><LayoutGrid></LayoutGrid>Theme</Button>
             </PopoverTrigger>
             <PopoverContent>
-            <h2 className=' font-bold text-sm mb-3 '>Select Your desired Theme Color :</h2>
+                <h2 className=' font-bold text-sm mb-3 '>Select Your desired Theme Color :</h2>
                 <div className='grid grid-cols-5 gap-3'>
                     {
                         colors.map((color, index) => (
-                            <div className={`h-5 w-5 rounded-full cursor-pointer hover:border-black border hover:scale-105 ${selectedColor==color&&'border border-black'}`} key={index}
+                            <div className={`h-5 w-5 rounded-full cursor-pointer hover:border-black border hover:scale-105 ${selectedColor == color && 'border border-black'}`} key={index}
                                 style={{
                                     background: color
-                                }} onClick={()=>onColorSelect(color)}>
+                                }} onClick={() => onColorSelect(color)}>
 
                             </div>
                         ))
                     }
 
                 </div>
+                <label htmlFor='themeColor'>Enter your desired color Hexcode</label>
+                <Input
+                    defaultValue={resumeInfo?.themeColor}
+                    name='themeColor'
+                    onChange={(e) => onColorSelect(e.target.value)}
+                    placeholder="Enter Hex Code (e.g., #FF5733)"
+                />
+
 
             </PopoverContent>
         </Popover>
--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,12 +1,27 @@
 import Header from '@/components/custom/Header'
 import { UserButton } from '@clerk/clerk-react'
 import React from 'react'
+import DecryptedText from '@/components/ui/DecryptedText'
+
 
 export default function Home() {
   return (
-    <div>
-    <Header></Header>
-      This is the home page
+    <div className='h-[1080px] w-full bg-black' >
+      <Header></Header>
+      
+      <div className='flex justify-center'>
+        <DecryptedText
+          text="<AI RESUME BUILDER/>"
+          animateOn="view"
+          revealDirection="start"
+          speed={100}
+          maxIterations={10}
+          sequential={true}
+          className='text-bold text-[5rem] uppercase font-extrabold text-white '
+          encryptedClassName='text-xl text-black'
+        />
+      </div>
+
     </div>
   )
 }
