6ac23fc6c61097f0940f09710c308a3196dca960
VaibhavDangaich <Vai1234dang@gmail.com>
2025-04-15 20:04:00 +0530

    --

--- ai-resume-admin/config/server.js
+++ ai-resume-admin/config/server.js
@@ -1,10 +1,17 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://ai-resume-builder-1-7vfs.onrender.com',
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  allowedHosts: ['ai-resume-builder-1-7vfs.onrender.com'],
 });
