86a46c441cc501763e216eea42c79710c33695eb
VaibhavDangaich <Vai1234dang@gmail.com>
2025-04-15 22:33:03 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
-    
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['ai-resume-builder-1-7vfs.onrender.com'],
   },
 })
